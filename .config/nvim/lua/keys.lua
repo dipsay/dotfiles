be7ba61731d25a3bf3cea43f32f86eb95e73ebf5
dipsay <dipsay68@gmail.com>
2023-02-18 16:47:32 -0500

    fixed keys

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -2,10 +2,6 @@ local function map(m, k, v)
     vim.keymap.set(m, k, v, { silent = true })
 end
 
-map("n", "<leader>n", "<CMD>bnext<CR>")
-map("n", "<leader>p", "<CMD>bprevious<CR>")
-map("n", "<leader>d", "<CMD>bdelete<CR>")
-
 map("n", "<leader>fr", "<CMD>Telescope oldfiles<CR>")
 map("n", "<leader>ff", "<CMD>Telescope find_files<CR>")
 map("n", "<leader>fw", "<CMD>Telescope live_grep<CR>")
